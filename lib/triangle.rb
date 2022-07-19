bb22ad67214044699de8e1dbd4c583dfeac7dfee
alextaylor <alextaylor515@gmail.com>
2022-07-19 15:47:52 -0400

    commit tree 3060591a5f0129e6a103087e8c969cac6686f4d2

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_accessor :kind
+  def initialize side1,side2,side3
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  def kind 
+    narr = [@side1,@side2,@side3]
+    if narr.find {|side| side<=0}
+      raise TriangleError
+    elsif @side1 + @side2 <= @side3 || @side1 + @side3 <= @side2 || @side2 + @side3 <= @side1
+      raise TriangleError
+    elsif narr.uniq.length == 1
+      return :equilateral
+    elsif narr.uniq.length == 2
+      return :isosceles
+    elsif narr.uniq.length == 3
+      return :scalene
+
+    end
+
+  end
+
+  class TriangleError < StandardError
+    
+  end
 end
